0c251301c3dc7c390e28cec839d6420a3bd1c7a8
Colin Pham <ctpham6@dons.usfca.edu>
2024-10-05 01:45:03 -0700

    Document class fixed

--- submission.py
+++ submission.py
@@ -12,20 +12,16 @@ def print_cluster(cluster_to_print, token_per_row = 2):
     for document in cluster_to_print.members:
         token_num = 1
         print("")
-        print("")
         print("Document", doc, "of class " + str(document.true_class))
-        print("")
-        print("")
         for token in document.tokens:
             if token_num % token_per_row == 0:
-                print(token_num, ": " + str(token) + " of value", document.tokens[token])
+                print(token_num, ": " + str(token) + " =", document.tokens[token])
             else:
-                print(token_num, ": " + str(token) + " of value", document.tokens[token], "          ", end=" ")
+                print(token_num, ": " + str(token) + " =", document.tokens[token], "          ", end=" ")
             token_num += 1
         doc += 1
         print("")
         print("__________________________________")
-        print("")
 
 # Here is my submission for Project 3
 # Lets create some documents. 5 positive documents and 5 negative documents of length 100 should do
@@ -33,12 +29,12 @@ data = create_docs(5, 5)
 documents = []
 
 for pos_word_set in data[0]:
-    doc_to_add = Document()
+    doc_to_add = Document(true_class='pos')
     doc_to_add.add_tokens(pos_word_set)
     documents.append(doc_to_add)
 
 for neg_word_set in data[1]:
-    doc_to_add = Document()
+    doc_to_add = Document(true_class='neg')
     doc_to_add.add_tokens(neg_word_set)
     documents.append(doc_to_add)
 
@@ -48,5 +44,5 @@ for neg_word_set in data[1]:
 # Each token is printed out like this:  "[Token #] [Token String] of value [Token Value]"
 cluster_list = k_means(2, ['pos', 'neg'], documents)
 for cluster in cluster_list:
-    print_cluster(cluster, token_per_row = 100)
+    print_cluster(cluster, token_per_row = 500)
 print("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
