8f624805c34801301f2656c7d8be60d3aec9f4c9
Colin Pham <ctpham6@dons.usfca.edu>
2024-10-06 02:55:11 -0700

    Docs created

--- Classifier.py
+++ Classifier.py
@@ -1,5 +1,8 @@
 from Cluster import *
 from Document import *
+from make_dataset import create_docs
+from submission import doc_to_add
+
 
 def classify(clusters, item) :
     dist = 10000
@@ -15,6 +18,19 @@ def classify(clusters, item) :
 def five_fold_cross_validation(nwords, nelements) :
     pass
     ## generate nelements documents of each type (pos and neg), with nwords words in each doc.
+    documents = []
+    word_sets = create_docs(nelements, nelements, nwords)
+
+    for set in word_sets[0] :
+        doc_to_add = Document(true_class='pos')
+        doc_to_add.add_tokens(set)
+        documents.append(doc_to_add)
+        
+    for set in word_sets[1] :
+        doc_to_add = Document(true_class='neg')
+        doc_to_add.add_tokens(set)
+        documents.append(doc_to_add)
+
     # divide the documents into 5 folds.
     ## for i = 1 to 5
     # cluster 80% of the documents. (four folds)
