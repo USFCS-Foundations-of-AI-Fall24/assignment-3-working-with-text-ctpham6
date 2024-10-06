8d295949d2de562b731c384b90b1665e59951fd1
Colin Pham <ctpham6@dons.usfca.edu>
2024-10-06 16:55:37 -0700

    Homogeneity Dixes'

--- Loader.py
+++ Loader.py
@@ -45,7 +45,7 @@ def convert_to_lowercase(token) :
 
 
 
-## homogeneity: for each cluster, what fraction of the cluster is comprised of the largest class?
+## Calculates the fraction of the cluster is comprised of the largest class?
 # call this like so:
 # result = k_means(2, ['pos','neg'], positive_docs + negative_docs)
 # compute_homogeneity(result, ['pos','neg'])
@@ -53,27 +53,31 @@ def compute_homogeneity(list_of_clusters, list_of_classes) :
 
     # hlist will be the homogeneity for each cluster.
     hlist = []
+    hlist_index = 0
 
-    largest_class = list_of_classes[0]
-    count = 1
-    i = 1
-    while i < len(list_of_classes) :
-        if largest_class == list_of_classes[i]:
-            count += 1
-        else:
-            if count == 1:
-                largest_class = list_of_classes[i]
-            else:
-                count -= 1
-
-    i = 0
+    # Goes through every cluster to find the largest class in each of them
     for cluster in list_of_clusters :
-        biggest_class_count = 0
-        for document in cluster.members:
-            if document.true_class == largest_class:
-                biggest_class_count += 1
-        hlist[i] = biggest_class_count / len(cluster.members)
-        i += 1
+        largest_class = None
+        count = 0
+        class_count = []
+        for class_type in list_of_classes :
+            class_count[list_of_classes.index(class_type)] = 0
+        for doc in cluster.members:
+            if doc.true_class in list_of_classes :
+                class_count[list_of_classes.index(doc.true_class)] += 1
+                if count == 0:
+                    largest_class = doc.true_class
+                else:
+                    if largest_class != doc.true_class:
+                        count -= 1
+                    else:
+                        count += 1
+        # my own decision to use the first class as the largest class if there is an equal amount of them
+        if count == 0 :
+            hlist[hlist_index] = round(class_count[0] / len(cluster.members), 2)
+        else :
+            hlist[hlist_index] = round(class_count[list_of_classes.index(largest_class)] / len(cluster.members), 2)
+        hlist_index += 1
 
     return hlist
 
@@ -87,8 +91,8 @@ def compute_completeness(list_of_clusters, list_of_classes) :
 
     # clist will be the completeness for each cluster.
     clist = []
-
     clist_index = 0
+
     for cluster in list_of_clusters :
         dom_class_count = 0
         dominant_class = cluster.true_class
