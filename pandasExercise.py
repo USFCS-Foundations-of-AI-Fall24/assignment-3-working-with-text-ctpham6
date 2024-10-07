75b306f878ccd351d673a57c962cf8357096fb1a
Colin Pham <ctpham6@dons.usfca.edu>
2024-10-06 23:53:34 -0700

    pandas finished

--- pandasExercise.py
+++ pandasExercise.py
@@ -1,6 +1,5 @@
-import numpy as np
-
 import pandas as pd
+import numpy as np
 
 
 # Write a function that will read in the breast cancer dataset and print out the classification column, which is the left-most column.
@@ -18,19 +17,26 @@ def compute_common_age_menopause(data) :
     table = pd.read_csv(data)
     data_frame = pd.DataFrame(table)
     filtered_table = data_frame[data_frame['no-recurrence-events'] == 'recurrence-events']
-    filtered_table = filtered_table[filtered_table['premeno'] != "premeno"]
     print("Out of all the patients with recurrences...")
     print(filtered_table['30-39'].value_counts().idxmax(), "is the most common age")
+    print(filtered_table['premeno'].value_counts().idxmax(), "is TECHNICALLY the most common menopause value")
+    print("If we exclude premenopause patients...")
+    filtered_table = filtered_table[filtered_table['premeno'] != "premeno"]
     print(filtered_table['premeno'].value_counts().idxmax(), "is the most common menopause")
 
 # Write a function that plots the number of recurrences for each age group.
 def plot_age_recurrences(data):
-    pass
+    table = pd.read_csv(data)
+    data_frame = pd.DataFrame(table)
+    filtered_table = data_frame[data_frame['no-recurrence-events'] == 'recurrence-events']
+    print("The following is the amount of age groups that have recurrences:")
+    print(filtered_table['30-39'].value_counts())
 
-# view_classifications('breast-cancer.data')
-# print("-----------------------------")
-# compute_zeroR('breast-cancer.data')
-# print("-----------------------------")
-# compute_common_age_menopause('breast-cancer.data')
-# print("-----------------------------")
+view_classifications('breast-cancer.data')
+print("-----------------------------")
+compute_zeroR('breast-cancer.data')
+print("-----------------------------")
+compute_common_age_menopause('breast-cancer.data')
+print("-----------------------------")
 plot_age_recurrences('breast-cancer.data')
+print("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
