81c7be9ed1baa530658fdb25d04531030ed3f955
Colin Pham <ctpham6@dons.usfca.edu>
2024-10-06 20:19:43 -0700

    k-means test expended

--- test_Cluster.py
+++ test_Cluster.py
@@ -1,6 +1,9 @@
+from lib2to3.btm_utils import tokens
 from unittest import TestCase
 from Cluster import *
 from Document import *
+from make_dataset import create_docs
+
 
 class TestCluster(TestCase):
     def test_calculate_centroid(self):
@@ -20,18 +23,16 @@ class TestCluster(TestCase):
         print(str(cluster.calculate_centroid()))
 
     def test_kmeans(self):
-        d = Document(true_class='pos')
-        d.add_tokens(['cat', 'dog', 'fish'])
-        d2 = Document(true_class='pos')
-        d2.add_tokens(['cat', 'dog', 'fish'])
-        d3 = Document(true_class='neg')
-        d3.add_tokens(['bunny', 'lizard', 'octopus'])
-        d4 = Document(true_class='pos')
-        d4.add_tokens(['the', 'quick', 'brown', 'fox', 'skibidi'])
-        d5 = Document(true_class='pos')
-        d5.add_tokens(['cat', 'dog', 'fish', 'toilet'])
-        d6 = Document(true_class='neg')
-        d6.add_tokens(['bunny', 'lizard', 'octopus', 'bop', 'bop', 'yes', 'yes'])
-        print(k_means(2, ['pos', 'neg'], [d,d2,d3,d4,d5,d6]))
+        pos_sets, neg_sets = create_docs(50, 50, 100)
+        docs = []
+        for set in pos_sets:
+            doc_to_add = Document(true_class='pos')
+            doc_to_add.add_tokens(set)
+            docs.append(doc_to_add)
+        for set in neg_sets:
+            doc_to_add = Document(true_class='neg')
+            doc_to_add.add_tokens(set)
+            docs.append(doc_to_add)
+        print(k_means(2, ['pos', 'neg'], docs))
 
 
